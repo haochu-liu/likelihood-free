9b2d28f96d7620cf4c4e128ef6c71a5cb4c3d57b
haochu-liu <liu_haochu@outlook.com>
2025-10-28 16:56:44 +0000

    new plot and parameters

--- example/L-V_model_run.R
+++ example/L-V_model_run.R
@@ -1,4 +1,7 @@
-theta <- c(1.0, 0.01, 0.6)
+library(ggplot2)
+
+
+theta <- c(1.0, 0.005, 0.6)
 state <- c(100, 50)
 time_points <- c(0, Measurement_times)
 S <- matrix(c(
@@ -9,3 +12,12 @@ S <- matrix(c(
 
 set.seed(100)
 results <- Lotka_Volterra(theta, state, time_points, S)
+
+ggplot(results, aes(x = Time)) +
+  geom_line(aes(y = Prey, color = "Prey")) +
+  geom_line(aes(y = Predator, color = "Predator")) +
+  geom_point(aes(y = Prey, color = "Prey")) +
+  geom_point(aes(y = Predator, color = "Predator")) +
+  labs(title = "Lotka-Volterra Simulation",
+       y = "Population Size", x = "Time") +
+  scale_color_manual(values = c("Prey" = "darkred", "Predator" = "darkblue"), name = "Species")
