0f55e3c89dca526f825e487afad28476d95ddcc4
LIU <u2008181@live.warwick.ac.uk>
2025-10-30 16:25:06 +0000

    finish sl smc

--- BSL/SL_SMC.R
+++ BSL/SL_SMC.R
@@ -7,27 +7,31 @@
 #' @param N Number of particles for SMC.
 #' @param theta_d Dimension of parameter.
 #' @param obs A vector of the observed statistics.
-#' @param prior_func A function to sample parameters from prior.
+#' @param prior_sampler A function to draw samples form prior.
+#' @param prior_func A density function of prior (log density).
 #' @param sample_func A function which takes theta and M and return sample mean and variance.
 #' @param sigma A scale parameter for the Gaussian proposal in move step.
 #' @param theta_history Default theta_history = FALSE, if TRUE, return all particles in history.
 #' @return A vector of parameters from the BSL posterior.
-SL_MCMC <- function(M, alpha, N, theta_d, obs, prior_func, sample_func, sigma,
-                    theta_history=FALSE) {
+SL_SMC <- function(M, alpha, N, theta_d, obs, prior_sampler, prior_func,
+                   sample_func, sigma, theta_history=FALSE) {
   theta_mat <- matrix(NA, nrow=theta_d, ncol=N)
+  iter_max <- 50
   if (theta_history) {
-    iter_max <- 100
     theta_history_mat <- matrix(NA, nrow=(iter_max*theta_d), ncol=N)
   }
   mu_mat <- matrix(NA, nrow=length(obs), ncol=N)
   sigma_array <- array(data = NA, dim = c(length(obs), length(obs), N))
   weight_vec <- rep(-log(N), N)
   ess_flat <- ESS_weight(weight_vec)
+  q_sigma <- sigma * diag(theta_d)
   gamma_old <- 0
+  iter <- 1
+  gamma_vec <- c(gamma_old)
 
   # Initialization
   for (n in 1:N) {
-    theta_n <- prior_func()
+    theta_n <- prior_sampler()
     sample_sta <- sample_func(theta_n, M)
 
     theta_mat[, n] <- theta_n
@@ -79,13 +83,50 @@ SL_MCMC <- function(M, alpha, N, theta_d, obs, prior_func, sample_func, sigma,
     # Resample
     prob_vec <- exp(weight_vec)
     resample_index <- sample(1:N, size=N, replace=TRUE, prob=prob_vec)
-    theta_mat <- theta_mat[, resample_index]
-    mu_mat <- mu_mat[, resample_index]
-    sigma_array <- sigma_array[, , resample_index]
+    theta_mat <- theta_mat[, resample_index, drop=FALSE]
+    mu_mat <- mu_mat[, resample_index, drop=FALSE]
+    log_likelihood <- log_likelihood[resample_index]
+    sigma_array <- sigma_array[, , resample_index, drop=FALSE]
+    weight_vec <- rep(-log(N), N)
 
     # Move
+    for (n in 1:N) {
+      theta_new <- theta_mat[, n] + as.vector(rmvnorm(n=1, sigma=q_sigma))
+      theta_new <- max(0, theta_new)
+      stats_new <- sample_func(theta_new, M)
+      sl_new <- dmvnorm(x=obs,
+                        mean=stats_new$mean, sigma=stats_new$sigma, log=TRUE)
 
+
+      log_alpha <- sl_new + prior_func(theta_new) -
+        log_likelihood[n] - prior_func(theta_mat[, n] )
+      log_alpha <- min(0, log_alpha)
+      log_u <- log(runif(1))
+
+      if (log_u < log_alpha) {
+        theta_mat[, n] <- theta_new
+        mu_mat[, n] <- stats_new$mean
+        sigma_array[, , n] <- stats_new$sigma
+      }
+    }
+
+    # Update
+    gamma_old <- gamma_new
+    gamma_vec <- c(gamma_vec, gamma_old)
+    if (theta_history) {
+      theta_history_mat[(1+(iter-1)*theta_d):(iter*theta_d), ] <- theta_mat
+    }
+    iter <- iter + 1
+    if (iter >= iter_max) {
+      break
+    }
+  }
+
+  if (theta_history) {
+    return(list(theta = theta_history_mat,
+                gamma = gamma_vec))
+  } else {
+    return(list(theta = theta_mat,
+                gamma = gamma_vec))
   }
 }
-
-
--- BSL/test.R
+++ BSL/test.R
@@ -5,8 +5,12 @@ set.seed(100)
 
 obs <- rnbinom(1, size=5*n, prob=0.5)
 
-prior_func <- function(){
+prior_sampler <- function(){
   return(rgamma(1, shape=2, rate=0.5))
+}
+
+prior_func <- function(theta){
+  dgamma(theta, shape=2, rate=0.5, log=TRUE)
 }
 
 sample_func <- function(theta, M) {
@@ -20,5 +24,8 @@ alpha <- 0.9
 N <- 100
 theta_d <- 1
 obs
+sigma <- 0.1
 theta_history=FALSE
 
+results <- SL_SMC(M, alpha, N, theta_d, obs, prior_sampler, prior_func,
+                  sample_func, sigma)
