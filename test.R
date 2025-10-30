53b79af9609d28e08eb7c86c55c5ec35afbf23b5
haochu-liu <liu_haochu@outlook.com>
2025-10-30 15:47:49 +0000

    debug first three parts

--- test.R
+++ test.R
@@ -1,16 +1,24 @@
-M <- 25
-alpha <- 0.9
-N <- 100
-theta_d <- 1
-obs
+library(mvtnorm)
+library(matrixStats)
+set.seed(100)
+
+
+obs <- rnbinom(1, size=5*n, prob=0.5)
 
 prior_func <- function(){
   return(rgamma(1, shape=2, rate=0.5))
 }
 
 sample_func <- function(theta, M) {
-  s <- rpois(M, theta*n)
+  s <- rpois(M, theta*20)
   return(list(mean=c(mean(s)),
               sigma=matrix(var(s), ncol=1, nrow=1)))
 }
+
+M <- 25
+alpha <- 0.9
+N <- 100
+theta_d <- 1
+obs
+theta_history=FALSE
 
