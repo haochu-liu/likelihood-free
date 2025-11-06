7bd7256957845b02803f7ea010b86d31eb6768bb
haochu-liu <liu_haochu@outlook.com>
2025-11-06 14:33:24 +0000

    q_sigma as input

--- example/cauchy_bimodal.R
+++ example/cauchy_bimodal.R
@@ -2,6 +2,7 @@ library(moments)
 library(mvtnorm)
 library(coda)
 library(matrixStats)
+setwd("C:/Users/u2008181/likelihood-free/example")
 source("../BSL/SL_MCMC.R")
 source("../BSL/SL_AM.R")
 source("../BSL/SL_SMC.R")
@@ -17,21 +18,19 @@ sample_mixture_cauchy <- function(theta = 5, gamma = 1, n = 20) {
 
 summary_stats <- function(y) {
   mean_y <- mean(y)
-  median_y <- median(y)
+  # median_y <- median(y)
   sd_y <- sd(y)
   mad_y <- mad(y)
   skew_y <- moments::skewness(y)
-  kurt_y <- moments::kurtosis(y) + 3  # convert to Pearson kurtosis
-  bc <- (skew_y^2 + 1) / kurt_y       # bimodality coefficient
+  kurt_y <- moments::kurtosis(y) + 3
+  bc <- (skew_y^2 + 1) / kurt_y
 
-  data.frame(
-    mean = mean_y, median = median_y, sd = sd_y, mad = mad_y,
-    skew = skew_y, kurtosis = kurt_y, bimodality_coeff = bc
-  )
+  stats_vec <- c(mean_y, sd_y, mad_y, skew_y, kurt_y, bc)
+  return(stats_vec)
 }
 
 n <- 20
-obs <- rnbinom(1, size=5*n, prob=0.5)
+y_obs <- rnbinom(1, size=5*n, prob=0.5)
 
 iter <- 50000
 init_theta <- c(rgamma(1, shape=2, rate=0.5))
