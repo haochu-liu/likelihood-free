cc2aa615bfe5046e953ad6fec9cfdc4482073d04
haochu-liu <liu_haochu@outlook.com>
2025-11-27 13:35:10 +0000

    move target

--- BSL/SL_WF_SMC.R
+++ BSL/SL_WF_SMC.R
@@ -133,8 +133,8 @@ SL_WF_SMC <- function(M, alpha, N, N_sample, theta_d, obs, prior_sampler,
           sl_new <- dmvnorm(x=obs,
                             mean=stats_new$mean, sigma=stats_new$sigma, log=TRUE)
 
-          log_alpha <- sl_new + prior_func(theta_new) -
-            sl_old - prior_func(theta_mat[, n])
+          log_alpha <- gamma_new * sl_new + prior_func(theta_new) -
+            gamma_new * sl_old - prior_func(theta_mat[, n])
           log_alpha <- min(0, log_alpha)
           log_u <- log(runif(1))
 
