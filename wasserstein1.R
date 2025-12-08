5361a4a9f9abd9ac211952355404f49de2d71221
LIU <u2008181@live.warwick.ac.uk>
2025-12-08 14:42:53 +0000

    compare_acc_eff

--- wasserstein1.R
+++ wasserstein1.R
@@ -7,7 +7,7 @@
 #' @return W_1(P, Q)
 wasserstein1 <- function(X, q) {
   X_sorted <- sort(X)
-  # Example: q <- qnorm((1:n)/(n+1), mean = 0, sd = 1).
+  # Example: q <- qnorm((1:n)/(n+1), mean = 0, sd = 1)
   W1 <- mean(abs(X_sorted - q))
   return(W1)
 }
