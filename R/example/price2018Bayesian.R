e03fedca340c78740d05ad9e7ed7e00f5346ae51
haochu-liu <liu_haochu@outlook.com>
2026-01-13 12:48:32 +0000

    add setup

--- R/example/price2018Bayesian.R
+++ R/example/price2018Bayesian.R
@@ -2,6 +2,7 @@ library(mvtnorm)
 library(rjags)
 library(coda)
 library(matrixStats)
+library(rmatio)
 source("R/BSL/SL_MCMC.R")
 set.seed(100)
 
@@ -11,8 +12,12 @@ alpha <- 0.001
 beta <- 0.001
 N <- 100
 
+# Load obs data
+github_url <- "https://raw.githubusercontent.com/cdrovandi/Bayesian-Synthetic-Likelihood/master/Simple/data_poisson.mat"
+data_list <- read.mat(github_url)
+
 # Sampling and densities
-obs <- rpois(N, lambda)
+obs <- data_list$y
 lambda_vec <- seq(0, 50, length.out = 50001)
 prior_density <- dgamma(lambda_vec, shape=alpha, rate=beta)
 posterior_density <- dgamma(lambda_vec, shape=alpha+sum(obs), rate=beta+N)
@@ -39,13 +44,12 @@ init_theta <- c(rgamma(1, shape=alpha, rate=beta))
 prior_func <- function(theta){
   dgamma(theta, shape=alpha, rate=beta, log=TRUE)
 }
-sample_func <- function(theta, n) {
-  x <- rpois(n, theta)
-  return(list(mean=c(mean(x)),
+sample_func <- function(theta, n, N) {
+  x_mat <- matrix(rpois(n*N, theta), nrow=N, ncol=n)
+  s <- colMeans(x_mat)
+  return(list(mean=c(mean(s)),
               sigma=matrix(var(s), ncol=1, nrow=1)))
 }
-
-
 
 
 
