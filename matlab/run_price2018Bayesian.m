6c4c52a26ba6b943300edb7efe9b2b9c975db580
Haochu <haochu.liu@warwick.ac.uk>
2026-01-14 17:56:38 +0000

    run 10 times

--- matlab/run_price2018Bayesian.m
+++ matlab/run_price2018Bayesian.m
@@ -9,16 +9,18 @@ chain_est_var = zeros(100000, 80);
 chain_fix_var = zeros(100000, 80);
 
 for i = 1:8
+    fprintf('----------\n');
     for j = 1:10
         [mcmc_chain, acc_rate] = bayes_sl_simple_fix(y, 100000, n(i));
-        mcmc_acc1{i, "acc_rate.fix_var"} = acc_rate;
-        chain_fix_var(:, i) = mcmc_chain;
+        acc_fix_var(i, j) = acc_rate;
+        chain_fix_var(:, j+10*(i-1)) = mcmc_chain;
 
         if i > 1
             [mcmc_chain, acc_rate] = bayes_sl_simple(y, 100000, n(i));
-            mcmc_acc1{i, "acc_rate.est_var"} = acc_rate;
-            chain_est_var(:, i) = mcmc_chain;
+            acc_est_var(i, j) = acc_rate;
+            chain_est_var(:, j+10*(i-1)) = mcmc_chain;
         end
+        fprintf('Finish chain %d.\n', j);
     end    
     fprintf('Finish n = %d.\n', n(i));
 end
