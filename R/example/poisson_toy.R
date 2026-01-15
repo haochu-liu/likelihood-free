d25277c2288e209cdb39ff21807c6ba9bb24f70e
haochu-liu <liu_haochu@outlook.com>
2026-01-15 23:24:32 +0000

    poisson toy

--- R/example/poisson_toy.R
+++ R/example/poisson_toy.R
@@ -25,21 +25,19 @@ prior_func <- function(theta){
 
 sample_func <- function(theta, n) {
   repeat {
-    x_mat <- matrix(rpois(n*N, theta), nrow=N, ncol=n)
-    if (var(s) > 0) {
-      return(list(mean=rowMeans(x_mat),
-                  sigma=var(t(x_mat))))
-    }
+    x_mat <- matrix(rpois(n*N_val, theta), nrow=N_val, ncol=n)
+    return(list(mean=rowMeans(x_mat),
+                sigma=var(t(x_mat))))
   }
 }
 sample_func_fix_sigma <- function(theta, n) {
-  x_mat <- matrix(rpois(n*N, theta), nrow=N, ncol=n)
+  x_mat <- matrix(rpois(n*N_val, theta), nrow=N_val, ncol=n)
   return(list(mean=rowMeans(x_mat),
-              sigma=diag(30, nrow=N)))
+              sigma=diag(30, nrow=N_val)))
 }
 
 proposal <- function(theta_old){
-  sd_proposal <- sqrt(alpha + sum(y_obs)) / (beta + N)
+  sd_proposal <- sqrt(alpha + sum(y_obs)) / (beta + N_val)
   repeat {
     theta_new <- rnorm(1, mean=theta_old, sd=sd_proposal)
     if (theta_new > 0) {
@@ -48,72 +46,108 @@ proposal <- function(theta_old){
   }
 }
 
-# Fix T = 100000, change n
+# Fix T = 10000, change n
 set.seed(100)
-T_iter <- 100000
-mcmc_quality1 <- data.frame(n=c(1, 2, 5, 6, 7, 10, 15, 20),
-                            acc_rate.fix_var=NA,
-                            ess.fix_var=NA,
-                            var_log.fix_var=NA,
-                            acc_rate.est_var=NA,
-                            ess.est_var=NA,
-                            var_log.est_var=NA)
-for (i in 1:nrow(mcmc_quality1)) {
-  n <- mcmc_quality1$n[i]
+T_iter <- 10000
+n <- c(1, 2, 5, 6, 7, 10, 15, 20)
+N <- c(1, 2, 5, 6, 7, 10, 15, 20)
 
-  acc_rate_vec <- rep(NA, 10)
-  ess_vec <- rep(NA, 10)
-  for (j in 1:10) {
-    bsl_out <- SL_MCMC2(n, T_iter, s_obs, init_theta, prior_func, sample_func_fix_sigma,
-                        proposal, acc_rate=TRUE)
-    acc_rate_vec[j] <- bsl_out$acc_rate
-    ess_vec[j] <- as.numeric(effectiveSize(as.mcmc(bsl_out$theta[1, ])))
-  }
+acc_rate.fix_var <- matrix(NA, nrow=length(n), ncol=length(N))
+colnames(acc_rate.fix_var) <- N
+rownames(acc_rate.fix_var) <- n
 
-  mcmc_quality1$acc_rate.fix_var[i] <- mean(acc_rate_vec)
-  mcmc_quality1$ess.fix_var[i] <- mean(ess_vec)
+# acc_rate.est_var <- matrix(NA, nrow=length(n), ncol=length(N))
+# colnames(acc_rate.est_var) <- N
+# rownames(acc_rate.est_var) <- n
 
-  log_like_vec <- rep(NA, 100)
-  for (k in 1:100) {
-    stats_n <- sample_func_fix_sigma(lambda, n)
-    log_like_vec[k] <- dmvnorm(x=s_obs,
-                               mean=stats_n$mean,
-                               sigma=stats_n$sigma,
-                               log=TRUE)
-  }
-  mcmc_quality1$var_log.fix_var[i] <- var(log_like_vec)
+ess.fix_var <- matrix(NA, nrow=length(n), ncol=length(N))
+colnames(ess.fix_var) <- N
+rownames(ess.fix_var) <- n
 
-  if (n > 1) {
+# ess.est_var <- matrix(NA, nrow=length(n), ncol=length(N))
+# colnames(ess.est_var) <- N
+# rownames(ess.est_var) <- n
+
+var_log.fix_var <- matrix(NA, nrow=length(n), ncol=length(N))
+colnames(var_log.fix_var) <- N
+rownames(var_log.fix_var) <- n
+
+# var_log.est_var <- matrix(NA, nrow=length(n), ncol=length(N))
+# colnames(var_log.est_var) <- N
+# rownames(var_log.est_var) <- n
+
+
+for (i in 1:length(N)) {
+  N_val <- N[i]
+  for (j in 1:length(n)) {
+    n_val <- n[j]
+
     acc_rate_vec <- rep(NA, 10)
     ess_vec <- rep(NA, 10)
-    for (j in 1:10) {
-      bsl_out <- SL_MCMC2(n, T_iter, s_obs, init_theta, prior_func, sample_func,
+    for (k in 1:10) {
+      bsl_out <- SL_MCMC2(n_val, T_iter, y_obs[1:N_val], init_theta, prior_func, sample_func_fix_sigma,
                           proposal, acc_rate=TRUE)
-      acc_rate_vec[j] <- bsl_out$acc_rate
-      ess_vec[j] <- as.numeric(effectiveSize(as.mcmc(bsl_out$theta[1, ])))
+      acc_rate_vec[k] <- bsl_out$acc_rate
+      ess_vec[k] <- as.numeric(effectiveSize(as.mcmc(bsl_out$theta[1, ])))
     }
 
-    mcmc_quality1$acc_rate.est_var[i] <- mean(acc_rate_vec)
-    mcmc_quality1$ess.est_var[i] <- mean(ess_vec)
+    acc_rate.fix_var[j, i] <- mean(acc_rate_vec)
+    ess.fix_var[j, i] <- mean(ess_vec)
 
     log_like_vec <- rep(NA, 100)
     for (k in 1:100) {
-      stats_n <- sample_func(lambda, n)
-      log_like_vec[k] <- dmvnorm(x=s_obs,
+      stats_n <- sample_func_fix_sigma(lambda, n_val)
+      log_like_vec[k] <- dmvnorm(x=y_obs[1:N_val],
                                  mean=stats_n$mean,
                                  sigma=stats_n$sigma,
                                  log=TRUE)
     }
-    mcmc_quality1$var_log.est_var[i] <- var(log_like_vec)
-  }
+    var_log.fix_var[j, i] <- var(log_like_vec)
 
-  print(paste0("n = ", n, " finish."))
+    # if (n_val > 1) {
+    #   acc_rate_vec <- rep(NA, 10)
+    #   ess_vec <- rep(NA, 10)
+    #   for (k in 1:10) {
+    #     bsl_out <- SL_MCMC2(n_val, T_iter, y_obs[1:N_val], init_theta, prior_func, sample_func,
+    #                         proposal, acc_rate=TRUE)
+    #     acc_rate_vec[k] <- bsl_out$acc_rate
+    #     ess_vec[k] <- as.numeric(effectiveSize(as.mcmc(bsl_out$theta[1, ])))
+    #   }
+    #
+    #   acc_rate.est_var[j, i] <- mean(acc_rate_vec)
+    #   ess.est_var[j, i] <- mean(ess_vec)
+    #
+    #   log_like_vec <- rep(NA, 100)
+    #   for (k in 1:100) {
+    #     stats_n <- sample_func(lambda, n_val)
+    #     log_like_vec[k] <- dmvnorm(x=y_obs[1:N_val],
+    #                                mean=stats_n$mean,
+    #                                sigma=stats_n$sigma,
+    #                                log=TRUE)
+    #   }
+    #  var_log.est_var[j, i] <- var(log_like_vec)
+    # }
+
+    print(paste0("N = ", N_val, ", n = ", n_val, " finish."))
+  }
 }
 
 
+norm_ess.fix_var <- ess.fix_var
+for (i in 1:length(N)) {
+  N_val <- N[i]
+  for (j in 1:length(n)) {
+    n_val <- n[j]
+    norm_ess.fix_var[j, i] <- ess.fix_var[j, i] / (N_val * n_val)
+  }
+}
 
+toy_fix_var <- list(acc_rate=acc_rate.fix_var,
+                    ess=ess.fix_var,
+                    norm_ess=norm_ess.fix_var,
+                    var_log=var_log.fix_var)
 
-
+save(toy_fix_var, file="data/toy_fix_var.RData")
 
 
 
