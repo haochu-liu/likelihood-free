449ef82afd435a62949cacfe8dc3d5c8930fbfdc
haochu-liu <liu_haochu@outlook.com>
2026-01-19 12:20:27 +0000

    simulate ricker's model

--- R/Ricker/simulate_ricker.R
+++ R/Ricker/simulate_ricker.R
@@ -7,5 +7,15 @@
 #' @param T_iter The length of the data set
 #' @return The simulated data set of length T_iter.
 simulate_ricker <- function(theta, N_t, T_iter) {
+  y <- rep(NA, T_iter)
+  r <- exp(theta[1])
+  phi <- theta[2]
+  sigmae <- theta[3]
 
+  for (i in 1:T_iter) {
+    e_t <- rnorm(1, mean=0, sd=sigmae)
+    y[i] <- rpois(1, phi*r*N_t*exp(-N_t+e_t))
+  }
+
+  return(y)
 }
