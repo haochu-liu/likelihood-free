b9edb01302f1c293fde2fbe6ae2ef897e6eeee58
haochu-liu <liu_haochu@outlook.com>
2026-01-19 13:13:33 +0000

    modify ricker simulation

---
+++ R/Ricker/ricker_summstats.R
@@ -0,0 +1,10 @@
+#' Summary statistics for Ricker model
+#'
+#' ricker_summstats computes the summary statistics used in the Ricker model (Wood, 2010).
+#'
+#' @param x A vector of simulated data.
+#' @param y A vector of observed data.
+#' @return Summary statistics for the simulated data x, a vector of dim 13.
+ricker_summstats <- function(x, y) {
+
+}
--- R/Ricker/simulate_ricker.R
+++ R/Ricker/simulate_ricker.R
@@ -3,8 +3,8 @@
 #' simulate_ricker simulates one data set from the model.
 #'
 #' @param theta A three dimension vector for the parameters.
-#' @param N_t The starting population (equal to 1 in our application)
-#' @param T_iter The length of the data set
+#' @param N_t The starting population (equal to 1 in our application).
+#' @param T_iter The length of the data set.
 #' @return The simulated data set of length T_iter.
 simulate_ricker <- function(theta, N_t, T_iter) {
   y <- rep(NA, T_iter)
@@ -13,8 +13,8 @@ simulate_ricker <- function(theta, N_t, T_iter) {
   sigmae <- theta[3]
 
   for (i in 1:T_iter) {
-    e_t <- rnorm(1, mean=0, sd=sigmae)
-    y[i] <- rpois(1, phi*r*N_t*exp(-N_t+e_t))
+    N_t <- r * N_t * exp(-N_t + rnorm(1, mean=0, sd=sigmae))
+    y[i] <- rpois(1, phi * N_t)
   }
 
   return(y)
