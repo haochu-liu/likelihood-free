9f8d8ee5f8a05378d4d494d3c495b2517ddecdda
haochu-liu <liu_haochu@outlook.com>
2026-01-28 15:45:15 +0000

    rerun simulations

--- R/simARG/ClonalOrigin_sim.R
+++ R/simARG/ClonalOrigin_sim.R
@@ -1,4 +1,6 @@
 library(simARG)
+library(future.apply)
+library(progressr)
 
 
 set.seed(100)
@@ -8,20 +10,58 @@ param_mat <- matrix(NA, nrow=3, ncol=1000)
 summary_stats_mat <- matrix(NA, nrow=7, ncol=1000)
 
 param_mat[1, ] <- runif(1000, min=0, max=0.2)  # rho_site
-param_mat[2, ] <- runif(1000, min=0, max=20)   # delta
+param_mat[2, ] <- runif(1000, min=1, max=500)  # delta
 param_mat[3, ] <- runif(1000, min=0, max=0.2)  # theta_site
 
-for (i in 1:1000) {
-  result <- ClonalOrigin_pair_seq.simulator(tree,
-                                            param_mat[1, i],
-                                            param_mat[3, i],
-                                            100L,
-                                            param_mat[2, i],
-                                            100,
-                                            c(2L, 10L, 75L))
+handlers(global = TRUE)
+handlers("progress")
 
-  summary_stats_mat[, i] <- result
-  print(paste("Finish", i, "iteration."))
-}
+plan(multisession, workers = 10)
+
+with_progress({
+  p <- progressor(along = 1:1000)
+  results_list <- future_lapply(1:1000, function(i) {
+    result <- ClonalOrigin_pair_seq.simulator(
+      tree,
+      param_mat[1, i],
+      param_mat[3, i],
+      100000L,
+      param_mat[2, i],
+      100,
+      c(50L, 200L, 2000L)
+    )
+    p(message = sprintf("Finish %g iteration", i))
+
+    return(result)
+  }, future.seed = TRUE)
+})
+
+plan(sequential)
+
+summary_stats_mat <- do.call(cbind, results_list)
 
 
+plot(param_mat[1, ], summary_stats_mat[1, ], pch=4, col="darkred")
+points(param_mat[1, ], summary_stats_mat[2, ], pch=4, col="darkblue")
+points(param_mat[1, ], summary_stats_mat[3, ], pch=4, col="darkgreen")
+
+plot(param_mat[2, ], summary_stats_mat[1, ], pch=4, col="darkred")
+points(param_mat[2, ], summary_stats_mat[2, ], pch=4, col="darkblue")
+points(param_mat[2, ], summary_stats_mat[3, ], pch=4, col="darkgreen")
+
+plot(param_mat[3, ], summary_stats_mat[1, ], pch=4, col="darkred")
+points(param_mat[3, ], summary_stats_mat[2, ], pch=4, col="darkblue")
+points(param_mat[3, ], summary_stats_mat[3, ], pch=4, col="darkgreen")
+
+
+plot(param_mat[1, ], summary_stats_mat[4, ], pch=4, col="darkred")
+points(param_mat[1, ], summary_stats_mat[5, ], pch=4, col="darkblue")
+points(param_mat[1, ], summary_stats_mat[6, ], pch=4, col="darkgreen")
+
+plot(param_mat[2, ], summary_stats_mat[4, ], pch=4, col="darkred")
+points(param_mat[2, ], summary_stats_mat[5, ], pch=4, col="darkblue")
+points(param_mat[2, ], summary_stats_mat[6, ], pch=4, col="darkgreen")
+
+plot(param_mat[3, ], summary_stats_mat[4, ], pch=4, col="darkred")
+points(param_mat[3, ], summary_stats_mat[5, ], pch=4, col="darkblue")
+points(param_mat[3, ], summary_stats_mat[6, ], pch=4, col="darkgreen")
