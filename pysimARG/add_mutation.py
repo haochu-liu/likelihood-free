9c058561cd49d22a54d02065a0335f4b2e342398
haochu-liu <liu_haochu@outlook.com>
2026-02-12 16:54:58 +0000

    add_mutations

--- pysimARG/add_mutation.py
+++ pysimARG/add_mutation.py
@@ -52,25 +52,16 @@ def add_mutation(arg, theta_site):
     # Simulate the mutations at every node
     # Process edges from last to first (bottom-up in the tree)
     for i in range(len(arg.edge) - 1, -1, -1):
-        # Get mutations on this edge (sites that have mutations on edge i)
         edge_mutation = mutate_site[mutate_edge == i]
 
         parent_idx = int(arg.edge[i, 0]) - 1
         child_idx = int(arg.edge[i, 1]) - 1
 
-        # Get parent sequence
         parent_seq = arg.node_site[parent_idx, :].copy()
-
-        # Count mutations at each site (equivalent to R's tabulate)
         flip_counts = np.bincount(edge_mutation, minlength=len(parent_seq))
-
-        # XOR with whether flip count is odd
         parent_seq = np.logical_xor(parent_seq, flip_counts % 2 == 1)
 
-        # Get material range for this edge
         material_range = arg.edge_mat[i, :]
-
-        # Update child node sequence only where there's material
         arg.node_site[child_idx, material_range==True] = parent_seq[material_range==True]
     
     return arg
