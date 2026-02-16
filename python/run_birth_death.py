7f09a82ae6e1432bfbbbe407c4a0430ce7e46935
haochu-liu <liu_haochu@outlook.com>
2026-02-16 15:45:29 +0000

    change run_birth_death

---
+++ python/run_birth_death.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append('..')
+from pysimARG.birth_death_sim import birth_death_sim
+import numpy as np
+
+
+birth_death_matrix = np.zeros((100, 2))
+for i in range(1, 101):
+    np.random.seed(i)
+    result = birth_death_sim(100, 10)
+    birth_death_matrix[i-1, :] = result
+    print(f'Complete {i} iterations')
+
+np.savetxt("data/birth_death_matrix.csv", birth_death_matrix, delimiter=",", fmt='%d')
