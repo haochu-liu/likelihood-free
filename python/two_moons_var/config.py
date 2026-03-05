e478e22ca481582ecc33ff5529e5b9b2934915fd
haochu-liu <liu_haochu@outlook.com>
2026-03-05 17:23:53 +0000

    modify simulator for clt

---
+++ python/two_moons_var/config.py
@@ -0,0 +1,6 @@
+torch_device = "cpu"
+
+num_rounds = 5
+seeds = list(range(1, 11))
+num_posterior_samples = 1000
+abc_mcmc_eps = 0.2
---
+++ python/two_moons_var/two_moon_sim.py
@@ -0,0 +1,70 @@
+import torch
+import numpy as np
+from config import torch_device
+from sbi.utils.torchutils import BoxUniform
+
+
+mean_radius = 0.1
+sd_radius = 0.01
+baseoffset = 0.25
+
+prior_torch = BoxUniform(
+    low=-1 * torch.ones(2), high=1 * torch.ones(2), device=torch_device
+)
+
+x_o = torch.tensor([0, 0], device=torch_device)
+x_o = x_o.flatten()
+
+x_o_numpy = x_o.cpu().numpy()
+
+
+def simulator_torch(theta):
+    theta = theta.reshape(-1)
+    a = (torch.pi * (torch.rand(1) - 0.5)).to(torch_device)
+    r = mean_radius + torch.randn(1, device=torch_device) * sd_radius
+    p = torch.tensor(
+        [r * torch.cos(a) + baseoffset, r * torch.sin(a)], device=torch_device
+    )
+    z0 = -torch.abs(theta[0] + theta[1]) / torch.sqrt(torch.tensor(2.0))
+    z1 = (-theta[0] + theta[1]) / torch.sqrt(torch.tensor(2.0))
+    location = p + torch.tensor([z0, z1], device=torch_device) + torch.randn(2, device=torch_device) * 0.1 / torch.sqrt(theta[2])
+    summary = torch.cat((location, location**2), dim=0)
+    return summary
+
+
+def simulator_torch_batched(theta):
+    n_sim, data_dim = theta.shape
+    x_samples = torch.zeros((n_sim, data_dim))
+
+    for i in range(n_sim):
+        x_samples[i,] = simulator_torch(theta[i,])
+
+    return x_samples
+
+
+def simulator_numpy(theta):
+    theta = theta.reshape(-1)
+    a = np.array(np.pi * (np.random.random(1) - 0.5))
+    r = mean_radius + np.random.normal(loc=0, scale=1, size=1) * sd_radius
+    p = np.array([r * np.cos(a) + baseoffset, r * np.sin(a)])
+    z0 = -np.abs(theta[0] + theta[1]) / np.sqrt(2.0)
+    z1 = (-theta[0] + theta[1]) / np.sqrt(2.0)
+    location = p.reshape(-1) + np.array([z0, z1]) + np.random.normal(loc=0, scale=0.1/np.sqrt(theta[2]), size=2)
+    summary = np.concatenate((location, location**2), axis=0)
+    return summary
+
+
+def analytic_posterior_numpy(x_o, n_samples=1):
+    theta = np.zeros((n_samples, 2))
+    for i in range(n_samples):
+        p = simulator_numpy(np.zeros(2))
+        q = np.zeros(2)
+        q[0] = (p[0] - x_o[0]) * np.sqrt(2)
+        q[1] = (x_o[1] - p[1]) * np.sqrt(2)
+
+        if np.random.rand() < 0.5:
+            q[0] = -q[0]
+
+        theta[i, 0] = (q[0] - q[1]) / 2
+        theta[i, 1] = (q[0] + q[1]) / 2
+    return theta
