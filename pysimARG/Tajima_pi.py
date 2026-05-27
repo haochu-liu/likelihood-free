a6d0f5af0655a4e6af878e96d3b48ad973a35692
haochu-liu <liu_haochu@outlook.com>
2026-05-27 13:37:27 +0100

    wakeley pi2

--- pysimARG/Tajima_pi.py
+++ pysimARG/Tajima_pi.py
@@ -1,28 +1,38 @@
 import numpy as np
 
 
-def Tajima_pi(mat):
+def Tajima_pi(mat, Wakeley=False):
     """
     Tajima's pi estimator.
     
     Compute Tajima's pi estimator for a given incidence matrix.
+    And optionally, compute Wakeley's pi squared estimator if `Wakeley` is set to True.
     
     Parameters
     ----------
     mat : np.ndarray
         An incidence matrix where each row represents a sequence.
     
+    Wakeley : bool, optional
+        Whether to compute Wakeley's pi squared estimator, by default False
+
     Returns
     -------
-    float
-        Tajima's pi estimator.
+    dict
+        Tajima's pi estimator and optionally Wakeley's pi squared estimator.
     """
     n = mat.shape[0]
 
-    pi_sum = 0
+    pi_list = []
     for i in range(n):
         for j in range(i + 1, n):
-            pi_sum += np.sum(mat[i] != mat[j])
-    
-    pi = 2 * pi_sum / (n * (n - 1))
-    return pi
+            pi_list.append(np.sum(mat[i] != mat[j]))
+
+    pi = 2 * np.sum(pi_list) / (n * (n - 1))
+    result = {'pi': pi}
+
+    if Wakeley:
+        pi2 = 2 * np.sum([(x - pi)**2 for x in pi_list]) / (n * (n - 1))
+        result['pi2'] = pi2
+
+    return result
