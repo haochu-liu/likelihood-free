169a3b9134d67c7d559f62ff11ff7bc5edfc1b9c
haochu-liu <liu_haochu@outlook.com>
2026-06-23 16:46:02 +0100

    read staph

--- R/simARG/staph_clonal_tree.R
+++ R/simARG/staph_clonal_tree.R
@@ -12,7 +12,32 @@ t2$edge.length=t2$edge.length/e*e2
 
 # Plot both trees
 plot(t, show.tip.label = F)
-plot(t2, show.tip.label = F)
+plot(t2, show.tip.label = T)
+
+n_leaf = Ntip(t2)
+edge = t2$edge
+leaf_height = rep(0, n_leaf)
+names(leaf_height) = c(1:n_leaf)
+node_height = c(leaf_height, branching.times(t2))
+node_height = sort(node_height)
+
+# Change node index
+# Node 1 - 110: unchanged
+# Node 111 - 219: ordered by height
+node_order <- as.integer(names(node_height))
+edge[] <- match(edge, node_order)
+
+edge_full = matrix(NA, nrow=nrow(edge), ncol=3)
+edge_full[, 1:2] = edge
+edge_full[, 3] = t2$edge.length
+edge_full <- edge_full[order(edge_full[, 1]), ]
+
+write.table(edge_full, file="data/staph/clonal_edge_new.csv",
+            sep = ",", row.names=FALSE, col.names=FALSE)
+write.table(node_height, file="data/staph/clonal_node_height_new.csv",
+            sep = ",", row.names=FALSE, col.names=FALSE)
+write.table(t2$tip.label, file="data/staph/tip_names.csv",
+            sep = ",", row.names=FALSE, col.names=FALSE)
 
 # Save t2
 write.tree(phy=t2, file="data/staph/saureus_clonal.nwk")
